68c1a0e99f5205817106100ac0b87e17839f78cd
Wiktor Szewczyk <wiktorsz2002@gmail.com>
2024-05-10 09:13:19 +0200

    Initial commit with base files

---
+++ rtl/i2c/i2c_init.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/10/2024 09:10:48 AM
+// Design Name: 
+// Module Name: i2c_init
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module i2c_init(
+
+    );
+endmodule
