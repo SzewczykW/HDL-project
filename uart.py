eb5696d466d1c15aa1cc84a3176e658e92657e9d
Alex <aleksandersieklinski@gmail.com>
2024-06-10 19:42:48 +0200

    added numberOfBits to read request

--- uart.py
+++ uart.py
@@ -3,34 +3,24 @@ import argparse
 import pyftdi.serialext
 import time
 
-def validate_data(data):
-    return True
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-r', '--read', help='Read address')
+    parser.add_argument('-r', '--read', nargs=2, help='Read address')
     parser.add_argument('-w', '--write', nargs=2, help='Write address and data')
     args = parser.parse_args()
     brate = 230400
     url = 'ftdi://ftdi:232:AQ00RVZA/1'
     port = pyftdi.serialext.serial_for_url(url, baudrate=brate, bytesize=8, stopbits=1, parity='N', xonxoff=False, rtscts=False)
     if args.read:
-        addr = args.read
+        addr, nb = args.read
         print(f"Reading from {addr}")
-        data = port.read(int(addr, 16))
+        data = port.read(int(addr,nb))
         print(f"Data: {data}")
     if args.write:
         addr, data = args.write
         print(f"Writing {data} to {addr}")
         data_bytes = data.encode()
         port.write(data_bytes)
-        print("Reading data after writing")
-        nb = 5
-        data = port.read(nb)
-        print('-', data)
-        if validate_data(data):
-            print("Data is valid")
-        else:
-            print("Data is not valid")
     port.close()
 if __name__ == "__main__":
     main()
